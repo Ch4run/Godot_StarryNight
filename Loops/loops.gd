00ea0267e63a288d7fed019f76bd496655ee18e5
Bucsi Laszlo <114463739+Ch4run@users.noreply.github.com>
2025-02-28 20:18:43 +0100

    Updated functions
    - 1920*1080 full HD screen size
    - added 0.02 sec waiting time between iterations
    - fixed input (space keypress)
    - iterated screenshot name

--- Loops/loops.gd
+++ Loops/loops.gd
@@ -1,26 +1,24 @@
 extends Node2D
-@export var spawn_count: int = 200
+@export var spawn_count: int = 256
 var star_scene = preload("res://Loops/star.tscn")
+var picture_number = 1 # to iterate screenshot name
 
 func _ready() -> void:
 	for i in spawn_count:
 		var star = star_scene.instantiate()
 		add_child(star)
-		star.position = Vector2(randi_range(-290, 290), randi_range(-170, 170))
-		#star.position.x = randi_range(-290, 290)
-		#star.position.y = randi_range(-170, 170)
-		
-		#var star_size: float = randf_range(0.2, 1)
-		star.scale *= randf_range(0.2, 1.0)
-		#star.scale.x = star_size
-		#star.scale.y = star_size
+		star.position = Vector2(randi_range(-960, 960), randi_range(-540, 540))
+		star.scale *= randf_range(0.2, 1.4)
+		await get_tree().create_timer(0.02).timeout #wait a bit for the next iteration
 
 func _process(delta: float) -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if Input.is_action_just_pressed("screenshot"):
 		_take_screenshot()
 
 func _take_screenshot():
 	var viewport = get_viewport()
-	var img = viewport.get_texture().get_image()  # Jelenet képe
-	img.flip_y()  # Godot-ban a képek fejjel lefelé mentődnek, ezért megfordítjuk
-	img.save_png("d://godot_screensave.png")  # PNG-ként mentés
+	var img = viewport.get_texture().get_image()
+	img.flip_y()
+	var pic_path_name = str("d://godot_screensave", picture_number, ".png")
+	img.save_png(pic_path_name)
+	picture_number += 1
