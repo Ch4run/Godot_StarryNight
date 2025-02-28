744a90f0e7453ab4ecfcc0164d8776b23f54a4a2
Bucsi Laszlo <114463739+Ch4run@users.noreply.github.com>
2025-02-28 20:30:31 +0100

    Separated spawner loop to a method

--- Loops/loops.gd
+++ Loops/loops.gd
@@ -4,16 +4,20 @@ var star_scene = preload("res://Loops/star.tscn")
 var picture_number = 1 # to iterate screenshot name
 
 func _ready() -> void:
+	_start_spawner()
+
+func _process(delta: float) -> void:
+	# press Space to make a screenshot
+	if Input.is_action_just_pressed("screenshot"):
+		_take_screenshot()
+
+func _start_spawner():
 	for i in spawn_count:
 		var star = star_scene.instantiate()
 		add_child(star)
 		star.position = Vector2(randi_range(-960, 960), randi_range(-540, 540))
 		star.scale *= randf_range(0.2, 1.4)
 		await get_tree().create_timer(0.02).timeout #wait a bit for the next iteration
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("screenshot"):
-		_take_screenshot()
 
 func _take_screenshot():
 	var viewport = get_viewport()
